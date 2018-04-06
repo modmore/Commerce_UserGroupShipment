2fd8d445601676de8062c7e0a3cd7f2e3eec5906
Mark Hamstra <hello@markhamstra.com>
2018-04-07 01:02:52 +0200

    Lexicon sync

---
+++ core/components/commerce_usergroupshipment/lexicon/nl/default.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$_lang['commerce_usergroupshipment'] = 'Gebuikers groep zending';
+$_lang['commerce_usergroupshipment.description'] = 'Maakt een nieuw zending type beschikbaar, gebruikers groep zending, waarmee een gebruiker automatisch toegang krijgt tot een per-product ingestelde gebruikersgroep. Om dit te gebruiken voeg je het zending type eerst toe aan een verzendtype, en selecteer je daarna bij een product die het verzendtype gebruikt de gewenste gebruikersgroep.';
+$_lang['commerce.UserGroupShipment'] = 'Gebruiker toewijzen aan gebruikersgroep';
