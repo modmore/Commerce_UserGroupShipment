d76b69f172a07c901405872b049abbacc51707c4
Mark Hamstra <hello@markhamstra.com>
2018-05-28 15:02:39 +0200

    Lexicon sync

--- core/components/commerce_usergroupshipment/lexicon/de/default.inc.php
+++ core/components/commerce_usergroupshipment/lexicon/de/default.inc.php
@@ -1,5 +1,5 @@
 <?php
 
 $_lang['commerce_usergroupshipment'] = 'UserGroupShipment';
-$_lang['commerce_usergroupshipment.description'] = 'Enables a new Shipment type, UserGroupShipment, which will automatically assign a user to a per-product specific user group. To use, first add the shipment type to a delivery type, and then select the usergroup on a product that uses the assigned delivery type.';
-$_lang['commerce.UserGroupShipment'] = 'Assign User to User Group';
+$_lang['commerce_usergroupshipment.description'] = 'Ermöglicht eine neue Versandart, UserGroupShipment, die einen Benutzer automatisch einer produktbezogenen Benutzergruppe zuordnet. Zur Verwendung fügen Sie zunächst die Transportart zu einer Lieferart hinzu und wählen dann die Benutzergruppe für ein Produkt aus, das die zugeordnete Lieferart verwendet.';
+$_lang['commerce.UserGroupShipment'] = 'Benutzer der Benutzergruppe zuweisen';
