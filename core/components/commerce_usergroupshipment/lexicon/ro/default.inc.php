fdf2dbf5859946901d587b612be45b4a0ecfebce
Mark Hamstra <hello@markhamstra.com>
2019-08-15 15:39:32 +0200

    Sync translations

---
+++ core/components/commerce_usergroupshipment/lexicon/ro/default.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$_lang['commerce_usergroupshipment'] = 'UserGroupShipment';
+$_lang['commerce_usergroupshipment.description'] = 'Enables a new Shipment type, UserGroupShipment, which will automatically assign a user to a per-product specific user group. To use, first add the shipment type to a delivery type, and then select the usergroup on a product that uses the assigned delivery type.';
+$_lang['commerce.UserGroupShipment'] = 'Assign User to User Group';
